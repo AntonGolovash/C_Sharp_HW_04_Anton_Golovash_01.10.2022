bc11c012b449f27af31ba2c99b0e4cdb85111596
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-10-03 19:47:34 +0300

    0

--- C_Sharp_HW_04_Anton_Golovash_01.10.2022/Program.cs
+++ C_Sharp_HW_04_Anton_Golovash_01.10.2022/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary_HW_04;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace C_Sharp_HW_04_Anton_Golovash_01._10._2022
     {
         static void Main(string[] args)
         {
+            Treugolnik treugolnik = new Treugolnik(5);
+            treugolnik.PerimetrFigury();
+            treugolnik.PloshadFigury();
+
+            Kvadrat kvadrat = new Kvadrat(7);
+            kvadrat.PerimetrFigury();
+            kvadrat.PloshadFigury();
+
 
         }
     }
---
+++ ClassLibrary_HW_04/Kvadrat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_HW_04
+{
+    public class Kvadrat : GeometricheskayaFigura
+    {
+        readonly double storona;
+        public Kvadrat(double storona)
+        {
+            this.storona = storona;
+
+        }
+        public override double PerimetrFigury()
+        {
+            return storona * 4;
+        }
+
+        public override double PloshadFigury()
+        {
+            return Math.Pow(storona, 2);
+        }
+    }
+}
